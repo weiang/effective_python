13b426959b6686af2b786d4ac37cdbeaf8a0c1b9
angwei <angwei@baidu.com>
2017-01-13 15:47:32 +0800

    start chapter 4

---
+++ chap4.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: chap4.py
+Author: angwei(angwei@baidu.com)
+Date: 2017/01/13 15:05:53
+"""
+
+# Item 29
+
+class Resistor(object):
+    def __init__(self, ohms):
+        self.ohms = ohms
+        self.voltage = 0
+        self.current = 0
+
+
+class VoltageResistance(Resistor):
+    def __init__(self, ohms):
+        super(VoltageResistance, self).__init__(ohms)
+        self._voltage = 0
+
+    @property
+    def voltage(self):
+        return self._voltage
+
+    @voltage.setter
+    def voltage(self, voltage):
+        self._voltage = voltage
+        self.current = self._voltage / self.ohms
+
+
+class BoundedResistance(Resistor):
+    def __init__(self, ohms):
+        super(BoundedResistance, self).__init__(ohms)
+
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, ohms):
+        if ohms <= 0:
+            raise ValueError('%f ohms must be > 0' % ohms)
+        self._ohms = ohms
+
+
+class FixedResistance(Resistor):
+    @property
+    def ohms(self):
+        return self._ohms
+
+    @ohms.setter
+    def ohms(self, ohms):
+        if hasattr(self, '_ohms'):
+            raise AttributeError("Can't set attribute")
+        self._ohms = ohms
+
+
+def item29():
+    r = BoundedResistance(1e3)
+#       r.ohms = 0
+#    r2 = BoundedResistance(-5)
+    r3 = FixedResistance(5)
+#    r3.ohms = 10
+
+
+def main():
+    item29()
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
