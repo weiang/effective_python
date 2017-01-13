2be4763fbfd87db3ea0434f133e7cc9bae8e084e
angwei <angwei@baidu.com>
2017-01-13 09:32:40 +0800

    Initial commit

---
+++ chap3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: chap3.py
+Author: angwei(angwei@baidu.com)
+Date: 2017/01/13 09:30:49
+"""
+
+# Item 28
+class FrequencyList(list):
+    pass
+
